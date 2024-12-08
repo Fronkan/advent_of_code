b393835cec957f355df00c050a6ca3f39a5bcb2e
Meconer <mats.asson@gmail.com>
2024-12-08 14:01:21 +0100

    Day8

--- 2024/meconer-ocaml/bin/main.ml
+++ 2024/meconer-ocaml/bin/main.ml
@@ -11,5 +11,7 @@ Printf.printf "Day5 part 1 : %d\n" ( Day5.resultP1);
 Printf.printf "Day5 part 2 : %d\n" ( Day5.resultP2); *)
 (* Printf.printf "Day6 part 1 : %d\n" ( Day6.resultP1);
 Printf.printf "Day6 part 2 : %d\n" ( Day6.resultP2); *)
-Printf.printf "Day7 part 1 : %d\n" ( Day7.resultP1);
-Printf.printf "Day7 part 2 : %d\n" ( Day7.resultP2);
+(* Printf.printf "Day7 part 1 : %d\n" ( Day7.resultP1);
+Printf.printf "Day7 part 2 : %d\n" ( Day7.resultP2); *)
+Printf.printf "Day8 part 1 : %d\n" ( Day8.resultP1);
+Printf.printf "Day8 part 2 : %d\n" ( Day8.resultP2);
