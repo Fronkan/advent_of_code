8b47a4917853133db4386ecd5edfead5d1acf8b4
Meconer <mats.asson@gmail.com>
2024-12-10 17:15:45 +0100

    Day 9

--- 2024/meconer-ocaml/bin/main.ml
+++ 2024/meconer-ocaml/bin/main.ml
@@ -8,10 +8,12 @@ Printf.printf "Day3 part 2 : %d\n" ( Day3.resultP2);
 Printf.printf "Day4 part 1 : %d\n" ( Day4.resultP1);
 Printf.printf "Day4 part 2 : %d\n" ( Day4.resultP2);
 Printf.printf "Day5 part 1 : %d\n" ( Day5.resultP1);
-Printf.printf "Day5 part 2 : %d\n" ( Day5.resultP2); *)
-(* Printf.printf "Day6 part 1 : %d\n" ( Day6.resultP1);
-Printf.printf "Day6 part 2 : %d\n" ( Day6.resultP2); *)
-(* Printf.printf "Day7 part 1 : %d\n" ( Day7.resultP1);
-Printf.printf "Day7 part 2 : %d\n" ( Day7.resultP2); *)
+Printf.printf "Day5 part 2 : %d\n" ( Day5.resultP2);
+Printf.printf "Day6 part 1 : %d\n" ( Day6.resultP1);
+Printf.printf "Day6 part 2 : %d\n" ( Day6.resultP2);
+Printf.printf "Day7 part 1 : %d\n" ( Day7.resultP1);
+Printf.printf "Day7 part 2 : %d\n" ( Day7.resultP2);
 Printf.printf "Day8 part 1 : %d\n" ( Day8.resultP1);
-Printf.printf "Day8 part 2 : %d\n" ( Day8.resultP2);
+Printf.printf "Day8 part 2 : %d\n" ( Day8.resultP2); *)
+Printf.printf "Day9 part 1 : %d\n" ( Day9.resultP1);
+Printf.printf "Day9 part 2 : %d\n" ( Day9.resultP2);
